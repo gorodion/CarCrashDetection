4b3e4fb2182fb5d5acd2adf775202ad0e41c8069
daniilorel <daniilorel@yandex-team.ru>
2022-01-29 17:01:46 +0300

    feat main: delete unnecessary files and change main

---
+++ accidents_logger.py
@@ -0,0 +1,21 @@
+import cv2
+from config import *
+from typing import Tuple
+from collections import deque
+
+
+def save_accident(frames: deque, out_dir: str, fps: float)->None:
+    """
+    This function saves the accident to the needed file
+    :param frames: a deque of accident frames
+    :param out_dir: path to video with the accident
+    :param fps: fps in the video
+    :return:
+    """
+    shape = frames[0].shape
+    fwidth, fheight = shape[0], shape[1]
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(out_dir, fourcc, fps, (fwidth, fheight))
+    for frame in frames:
+        out.write(frame)
+    out.release()
